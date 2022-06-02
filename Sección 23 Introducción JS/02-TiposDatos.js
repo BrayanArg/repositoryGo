42aaba4c56f7acc45e5d0376c10ac3cc847f3de9
BrayanArg <brayanalexisruiz@gmail.com>
2022-06-01 21:29:33 -0500

    Introducción a JS y tipos de datos

---
+++ Sección 23 Introducción JS/02-TiposDatos.js
@@ -0,0 +1,106 @@
+/* // Tipo de dato String
+
+var nombre = "Carlos";
+
+console.log(typeof nombre);
+
+nombre = 10;
+
+console.log(typeof nombre);
+
+
+// tipo de dato númerico
+
+var numero = 1000;
+
+console.log(numero);
+
+
+// tipo de dato Object
+
+var objeto = {
+    nombre: "Juan",
+    apellido: "Perez",
+    telefono: "552238885"
+};
+
+console.log(objeto);
+
+// tipo de dato boolean (true, false)
+
+var bandera = false;
+
+console.log(bandera);
+
+// tipo de dato function
+
+function miFuncion() {}
+console.log(miFuncion);
+
+
+// tipo de dato symbol
+
+var simbolo = Symbol("mi simbolo");
+console.log(typeof simbolo);
+
+// tipo clase es una function
+
+class persona {
+    constructor(nombre, apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+console.log(typeof persona);
+
+// tipo de dato undefined
+
+var x;
+
+console.log(x);
+
+x = undefined;
+console.log(x);
+
+// tipo de dato null = ausencia de valor
+
+var y = null;
+console.log(typeof y);
+
+var autos = ['BMW', 'Audi', 'Volvo'];
+console.log(autos);
+console.log(typeof autos);
+
+var z = "";
+console.log(z);
+console.log(typeof z);
+
+*/
+
+/*var nombre = "Juan";
+var apellido = "Perez";
+
+var nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+
+var x = nombre + 367 + 763;
+console.log(x);
+
+x = nombre + (2 + 4);
+console.log(x);
+
+x = 2 + 4 + nombre;
+console.log(x);
+*/
+
+let nombre = "juan";
+console.log(nombre)
+
+const apellido = "Perez";
+
+let x, y;
+
+x = 10, y = 20;
+
+let z = x + y;

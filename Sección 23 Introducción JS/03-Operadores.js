77ecf09f245f0487feb06ca2a21b0c61254bfcf7
BrayanArg <brayanalexisruiz@gmail.com>
2022-06-02 21:02:20 -0500

    Operadores JS

---
+++ Sección 23 Introducción JS/03-Operadores.js
@@ -0,0 +1,120 @@
+/*
+let a = 3,
+    b = 2;
+let z = a + b;
+console.log("Resultado de la suma: " + z);
+
+z = a - b;
+console.log("Resultado de la resta: " + z);
+
+z = a * b;
+console.log("Resultado de la multiplicación: " + z);
+
+z = a / b;
+console.log("Resultado de la división: " + z);
+
+z = a % b;
+console.log("Resultado de operación módulo (Residuo): " + z);
+
+z = a ** b;
+console.log("Resultado de operador exponente: " + z);*/
+/*
+let a = 3,
+    b = 2;
+let = z = a + b;
+
+// Incremento
+// Pre-incremento(el operador ++ antes de la variable)
+
+z = ++a;
+console.log(a);
+console.log(z);
+
+//Post-incremento (el operatod ++ después de la variable)
+
+z = b++;
+console.log(b);
+console.log(z);
+
+// Decremento
+// Pre-decremento
+z = --a;
+console.log(a);
+console.log(z);
+
+// Post-decremento
+z = b--;
+console.log(b);
+console.log(z);
+*/
+/*
+let a = 3,
+    b = 2,
+    c = 1,
+    d = 4;
+let z = a * b + c / d;
+console.log(z);
+
+z = c + a * b / d;
+console.log(z);
+
+z = (c + a) * b / c;
+console.log(z);
+*/
+
+/*
+let a = 1;
+
+a += 3; //a = a + 3
+console.log(a);
+
+a -= 2; // a = a - 2;
+console.log(a);
+*/
+
+/*
+let a = 3,
+    b = 2,
+    c = "3";
+
+let z = a == c; //Se revisa el valor sin importar el tipo.
+console.log(z);
+
+z = a === c; //Revisa si los valores son iguales pero también los tipos.
+console.log(z);
+
+z = a != c; //Se revisa el valor sin importar el tipo.
+console.log(z);
+
+z = a !== c; //Revisa si los valores son iguales pero también los tipos.
+console.log(z);
+*/
+
+/*
+let a = 3,
+    b = 2,
+    c = "3";
+
+let z = a < b;
+console.log(z);
+
+z = a <= b;
+console.log(z);
+
+z = a > b;
+console.log(z);
+
+z = a >= b;
+console.log(z);
+*/
+
+
+// Ejercicio número par
+
+let a = 10;
+
+if (a % 2 == 0) {
+    console.log("Es un número par");
+} else {
+    console.log("Es un número impar");
+}
